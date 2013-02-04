38c4edc078459dfe2e408f625a37fbcf8fc82283
Greg Brockman <gdb@gregbrockman.com>
2013-02-03 18:53:47 -0800

    Fix printer.rb

--- example/printer.rb
+++ example/printer.rb
@@ -8,8 +8,6 @@ $log = Logger.new(STDOUT)
 $log.level = Logger::WARN
 
 module Bulldozer
-  Bulldozer::RPC.use_git_repo(__FILE__)
-
   class Printer
     include Bulldozer::RPC
 
@@ -31,18 +29,14 @@ def main
   end
   optparse.parse!
 
-  if ARGV.length == 0
-    EM.run do
-      Bulldozer::RabbitMQ.connect_async
-      Bulldozer.run_worker
-    end
-  elsif ARGV.length == 1
-    Bulldozer::RabbitMQ.connect_sync
-    Bulldozer::Printer.defer(:print, ARGV[0])
-  else
+  Bulldozer::RPC.use_git_repo(__FILE__)
+
+  if ARGV.length != 1
     puts optparse
-    return 1
   end
+
+  Bulldozer::RabbitMQ.connect_sync
+  Bulldozer::Printer.defer(:print, ARGV[0])
 
   return 0
 end
